ae2cf5b23b4b3fbba20a9f604031aacdf68cfab3
abruckman <abruckman09@gmail.com>
2017-05-02 13:44:01 -0700

    add props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,14 +1,16 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitleFactory = React.createFactory(MyTitle)
+
 var myFirstComponent = React.createClass({
   render: function(){
     return(
       React.DOM.div(null,
-        React.createElement(MyTitle),
-        React.createElement(MyTitle),
-        React.createElement(MyTitle),
-        React.createElement(MyTitle)
+        React.createElement(MyTitle, {title:"props are the best"}),
+        React.createElement(MyTitle, {title:"second thing"}),
+        React.createElement(MyTitle, {title:"third thing"}),
+        React.createElement(MyTitle, {title:"last thing"})
       )
     )
   }
@@ -19,7 +21,7 @@ var MyTitle = React.createClass({
   render: function(){
     return(
       div(null,
-        h1(null, 'check it out')
+        h1(null, this.props.title)
       )
     )
   }
