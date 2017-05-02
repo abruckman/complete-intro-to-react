a0f22175fee7a495975bdea952a772da87b5e1bd
abruckman <abruckman09@gmail.com>
2017-05-02 14:07:03 -0700

    add standard

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,27 +1,26 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var myFirstComponent = React.createClass({
-  render: function(){
-    return(
+  render: function () {
+    return (
       React.DOM.div(null,
-        React.createElement(MyTitle, {title:"props are the best"}),
-        React.createElement(MyTitle, {title:"second thing"}),
-        React.createElement(MyTitle, {title:"third thing"}),
-        React.createElement(MyTitle, {title:"last thing"})
+        React.createElement(MyTitle, { title: 'props are the best', 'color': 'peru'}),
+        React.createElement(MyTitle, { title: 'second thing', 'color': 'mediumaquamarine'}),
+        React.createElement(MyTitle, { title: 'third thing', 'color': 'tomato'}),
+        React.createElement(MyTitle, { title: 'last thing', 'color': 'rebeccapurple'})
       )
     )
   }
 })
 
-
 var MyTitle = React.createClass({
-  render: function(){
-    return(
+  render: function () {
+    return (
       div(null,
-        h1(null, this.props.title)
+        h1({style: {color: this.props.color}}, this.props.title)
       )
     )
   }
