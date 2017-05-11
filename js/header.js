2f41f29e0f650483c90a99e73909d29db28f9f0a
abruckman <abruckman09@gmail.com>
2017-05-11 16:58:02 -0700

    add redux for searchTerms

--- js/header.js
+++ js/header.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router'
+import { connect } from 'react-redux'
+import {setSearchTerm} from './actionCreators'
 
 class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+  handleSearchTermChange (event) {
+    this.props.dispatch(setSearchTerm(event.target.value))
+  }
   render () {
     let utilSpace
     if (this.props.showSearch) {
-      utilSpace = <input onChange={this.props.handleSearchTermChange} type='text' value={this.props.searchTerm} placeholder='Search' />
+      utilSpace = <input onChange={this.handleSearchTermChange} type='text' value={this.props.searchTerm} placeholder='Search' />
     } else {
       utilSpace = (
         <h2>
@@ -29,9 +38,15 @@ class Header extends React.Component {
 const {func, bool, string} = React.PropTypes
 
 Header.propTypes = {
-  handleSearchTermChange: func,
+  dispatch: func,
   showSearch: bool,
   searchTerm: string
 }
 
-export default Header
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Header)
--- js/search.js
+++ js/search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 // import preload from '../public/data.json'
 import ShowCard from './showCard'
 import Header from './header'
@@ -9,28 +10,17 @@ const Search = React.createClass({
     shows: arrayOf(shape({
       title: string,
       descriptions: string
-    }))
-  },
-  getInitialState () {
-    return {
-      searchTerm: ''
-    }
-  },
-  handleSearchTermChange (event) {
-    this.setState({searchTerm: event.target.value})
+    })),
+    searchTerm: string
   },
   render () {
     return (
       <div className='search'>
-        <Header
-          showSearch
-          searchTerm={this.state.searchTerm}
-          handleSearchTermChange={this.handleSearchTermChange}
-        />
+        <Header showSearch />
         <div>
           {this.props.shows
           .filter((show) => {
-            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0
           })
           .map((show) => {
             return (
@@ -43,4 +33,10 @@ const Search = React.createClass({
   }
 })
 
-export default Search
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Search)
