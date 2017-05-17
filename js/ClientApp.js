b50a90f276b07c15d16f6f794d74d65dd9aa5e5a
abruckman <abruckman09@gmail.com>
2017-05-17 15:16:47 -0700

    start universal rendering

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,35 +1,6 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { HashRouter, Match } from 'react-router'
-import { Provider } from 'react-redux'
-import store from './store'
-import Landing from './landing'
-import Search from './search'
-import Details from './details'
-import preload from '../public/data.json'
-import '../public/normalize.css'
-import '../public/style.css'
+import {render} from 'react-dom'
+import { HashRouter } from 'react-router'
+import App from './app'
 
-const App = React.createClass({
-  render () {
-    return (
-      <HashRouter>
-        <Provider store={store}>
-          <div className='app'>
-            <Match exactly pattern='/' component={Landing} />
-            <Match pattern='/search'component={(...props) => <Search shows={preload.shows} />} />
-            <Match
-              pattern='/details/:id'
-              component={(props) => {
-                const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
-                return <Details show={shows[0]} {...props} />
-              }}
-            />
-          </div>
-        </Provider>
-      </HashRouter>
-    )
-  }
-})
-
-render(<App />, document.getElementById('app'))
+render (<HashRouter><App /></HashRouter>, document.getElementById('app'))
---
+++ js/app.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Match } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './store'
+import Landing from './landing'
+import Search from './search'
+import Details from './details'
+import preload from '../public/data.json'
+import '../public/normalize.css'
+import '../public/style.css'
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div className='app'>
+        <Match exactly pattern='/' component={Landing} />
+        <Match pattern='/search'component={(...props) => <Search shows={preload.shows} />} />
+        <Match
+          pattern='/details/:id'
+          component={(props) => {
+            const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
+            return <Details show={shows[0]} {...props} />
+          }}
+        />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
